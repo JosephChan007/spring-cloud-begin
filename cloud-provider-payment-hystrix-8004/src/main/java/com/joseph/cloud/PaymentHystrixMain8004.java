240a18769c997e1afac03db7c577645302a7f873
JosephChan007 <zjzyp007@hotmail.com>
2020-06-12 00:14:27 +0800

    commit

--- cloud-provider-payment-hystrix-8004/src/main/java/com/joseph/cloud/PaymentHystrixMain8004.java
+++ cloud-provider-payment-hystrix-8004/src/main/java/com/joseph/cloud/PaymentHystrixMain8004.java
@@ -1,9 +1,12 @@
 package com.joseph.cloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,6 +15,22 @@ public class PaymentHystrixMain8004 {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8004.class, args);
+    }
+
+
+    /**
+     * Hystrix Dashboard 相关的配置
+     * 由于springcloud 升级的坑：
+     *      SerlvetRegistrationBean 因为 springboot 的默认路径不是 "/hystrix.stream" 需要在被监控的项目的主启动类中配置一下下面的servlet
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
