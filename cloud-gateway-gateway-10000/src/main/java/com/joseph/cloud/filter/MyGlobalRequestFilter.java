f767a4ca522a93b5e2532f6f3e0ac6d6ae49dc55
JosephChan007 <zjzyp007@hotmail.com>
2020-06-13 02:01:58 +0800

    commit

---
+++ cloud-gateway-gateway-10000/src/main/java/com/joseph/cloud/filter/MyGlobalRequestFilter.java
@@ -0,0 +1,35 @@
+package com.joseph.cloud.filter;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class MyGlobalRequestFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("****** MyGlobalRequestFilter Actived ******");
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (StringUtils.isBlank(uname)) {
+            log.warn("****** 用户名为空，非法用户 ******");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+}
